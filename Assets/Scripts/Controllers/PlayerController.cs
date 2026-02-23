9f126953514fcba858e9eeb60d338e355485930f
maxenced-dev <maxence.desceliers@gmail.com>
2026-02-23 14:29:32 +0100

    quick cleanup

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -1,119 +1,115 @@
-using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.Controls;
 
-public class PlayerController : MonoBehaviour
+namespace Controllers
 {
+    public class PlayerController : MonoBehaviour
+    {
     
-    //TODO empecher le joueur de rester collé au mur quand on se déplace dessus, mvt en l'air si besoin, limiter vitesse de chute
+        //TODO empecher le joueur de rester collé au mur quand on se déplace dessus, mvt en l'air si besoin, limiter vitesse de chute
     
-    private Vector2 moveInput;
-    private Rigidbody2D rb;
+        private Vector2 _moveInput;
+        private Rigidbody2D _rb;
     
     
-    private float coyoteTimeCounter;
-    private float jumpBufferCounter;
+        private float _coyoteTimeCounter;
+        private float _jumpBufferCounter;
 
-    [Header("Player")] [SerializeField] float jumpStrenght = 5f;
-    [SerializeField] private bool jumpButtonPressed = false;
-    [SerializeField] float playerSpeed = 5f;
-    [SerializeField] bool grounded = true;
-    [SerializeField] float coyoteTime = 0.2f;
-    [SerializeField] private float jumpCutMultiplier = 0.2f; // reduit la vitesse quand on relache le bouton saut
-    [SerializeField] private float jumpBufferTime = 0.2f;
-    private bool jumpButtonReleased = false;
+        [Header("Player")] [SerializeField] private float jumpStrength = 5f;
+        [SerializeField] private bool jumpButtonPressed = false;
+        [SerializeField] private float playerSpeed = 5f;
+        [SerializeField] private bool grounded = true;
+        [SerializeField] private float coyoteTime = 0.1f;
+        [SerializeField] private float jumpCutMultiplier = 0.5f; // reduit la vitesse quand on relache le bouton saut
+        [SerializeField] private float jumpBufferTime = 0.2f;
+        private bool _jumpButtonReleased = false;
 
-    [Header("Misc")] [SerializeField] private float groundCheckDistance = 0.5f;
-    [SerializeField] private LayerMask groundLayer;
-    [SerializeField] private Vector2 boxSize = new Vector2(0.8f, 0.2f);
+        [Header("Misc")] [SerializeField] private float groundCheckDistance = 0.5f;
+        [SerializeField] private LayerMask groundLayer;
+        [SerializeField] private Vector2 boxSize = new Vector2(0.8f, 0.2f);
 
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
-
-    void Update()
-    {
-
-    }
-
-    public void Jump(InputAction.CallbackContext ctx)
-    {
-        if (ctx.started)
+        private void Start()
         {
-            jumpButtonPressed = true;
-            jumpBufferCounter = jumpBufferTime;
+            _rb = GetComponent<Rigidbody2D>();
         }
-        
-        if (ctx.canceled)
-        {
-            jumpButtonReleased = true;
-        }
-    }
 
-    public void OnMove(InputAction.CallbackContext context)
-    {
-        moveInput = context.ReadValue<Vector2>();
-    }
-
-    public void OnLook(InputAction.CallbackContext context)
-    {
-        //viser ? 
-    }
-
-    private void FixedUpdate()
-    {
-        jumpBufferCounter -= Time.fixedDeltaTime;
-
-        //mvt
-        if (grounded)
+        public void Jump(InputAction.CallbackContext ctx)
         {
-            rb.linearVelocity = new Vector2(moveInput.x * playerSpeed, rb.linearVelocity.y);
-        }
-        else //mvt en l'air
-        {
-            rb.linearVelocity = new Vector2(moveInput.x * playerSpeed, rb.linearVelocity.y);
-          //TODO
-        }
-        
-        //coyoteTime
-        if (grounded)
-        {
-            coyoteTimeCounter = coyoteTime;
-        }
-        else
-        {
-            coyoteTimeCounter -= Time.fixedDeltaTime; //fixeddeltatime prcq on est dans fixedupdate
-        }
-        
-        //saut
-        if (coyoteTimeCounter > 0f && jumpBufferCounter > 0f)
-        {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpStrenght);
-            coyoteTimeCounter = 0f;
-            jumpBufferCounter = 0f;
-            jumpButtonPressed = false;
-        }
-        
-         //jump cut
-        if (jumpButtonReleased)
-        {
-            if (rb.linearVelocity.y > 0f)
+            if (ctx.started)
             {
-                rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * jumpCutMultiplier);
+                jumpButtonPressed = true;
+                _jumpBufferCounter = jumpBufferTime;
             }
-            jumpButtonReleased = false;
+        
+            if (ctx.canceled)
+            {
+                _jumpButtonReleased = true;
+            }
         }
+
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            _moveInput = context.ReadValue<Vector2>();
+        }
+
+        public void OnLook(InputAction.CallbackContext context)
+        {
+            //viser ? 
+        }
+
+        private void FixedUpdate()
+        {
+            _jumpBufferCounter -= Time.fixedDeltaTime;
+
+            //mvt
+            if (grounded)
+            {
+                _rb.linearVelocity = new Vector2(_moveInput.x * playerSpeed, _rb.linearVelocity.y);
+            }
+            else //mvt en l'air
+            {
+                _rb.linearVelocity = new Vector2(_moveInput.x * playerSpeed, _rb.linearVelocity.y);
+                //TODO
+            }
+        
+            //coyoteTime
+            if (grounded)
+            {
+                _coyoteTimeCounter = coyoteTime;
+            }
+            else
+            {
+                _coyoteTimeCounter -= Time.fixedDeltaTime; //fixeddeltatime prcq on est dans fixedupdate
+            }
+        
+            //saut
+            if (_coyoteTimeCounter > 0f && _jumpBufferCounter > 0f)
+            {
+                _rb.linearVelocity = new Vector2(_rb.linearVelocity.x, jumpStrength);
+                _coyoteTimeCounter = 0f;
+                _jumpBufferCounter = 0f;
+                jumpButtonPressed = false;
+            }
+        
+            //jump cut
+            if (_jumpButtonReleased)
+            {
+                if (_rb.linearVelocity.y > 0f)
+                {
+                    _rb.linearVelocity = new Vector2(_rb.linearVelocity.x, _rb.linearVelocity.y * jumpCutMultiplier);
+                }
+                _jumpButtonReleased = false;
+            }
         
         
-        RaycastHit2D hit = Physics2D.BoxCast(transform.position, boxSize, 0f, Vector2.down, groundCheckDistance, groundLayer);
-        grounded = hit.collider != null;
-    }
+            RaycastHit2D hit = Physics2D.BoxCast(transform.position, boxSize, 0f, Vector2.down, groundCheckDistance, groundLayer);
+            grounded = hit.collider;
+        }
     
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = grounded ? Color.green : Color.red;
-        Gizmos.DrawWireCube(transform.position + Vector3.down * groundCheckDistance, boxSize);
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = grounded ? Color.green : Color.red;
+            Gizmos.DrawWireCube(transform.position + Vector3.down * groundCheckDistance, boxSize);
+        }
     }
 }
