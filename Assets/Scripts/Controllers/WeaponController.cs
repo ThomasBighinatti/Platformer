06e7695dd41f116ec6672d3c034c3e7bcf737d1c
maxenced-dev <maxence.desceliers@gmail.com>
2026-02-26 02:01:06 +0100

    PlaceHolders for bow and arrow (gorgeous just gorgeous)
    those who cried / came >>>

--- Assets/Scripts/Controllers/WeaponController.cs
+++ Assets/Scripts/Controllers/WeaponController.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,12 +8,23 @@ namespace Controllers
 {
     public class WeaponController : MonoBehaviour
     {
+        
+        [Header("Settings")]
         [SerializeField] private List<GameObject> arrowList;
         [SerializeField, Range(0f, 1f)] private float deadZoneOnLook;
         
+        [SerializeField] private Sprite bowPlaceHolder1;
+        [SerializeField] private Sprite bowPlaceHolder2;
+        private SpriteRenderer _bowSpriteRenderer;
+        
         public static Vector2 Direction = Vector2.right;
         private Arrow _arrowScript;
-        
+
+        private void Start()
+        {
+            _bowSpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        }
+
         public void OnShoot(InputAction.CallbackContext context)
         {
             if (context.started && _arrowScript == null)
@@ -22,12 +35,20 @@ namespace Controllers
                 Vector3 rotation = new Vector3(0,0, angle);
                 _arrowScript = arrow.GetComponent<Arrow>();
                 _arrowScript.transform.eulerAngles = rotation;
+                
+                gameObject.transform.eulerAngles = rotation;
+                
+                //Placeholder
+                _bowSpriteRenderer.sprite = bowPlaceHolder2;
             }
 
             if (context.canceled && _arrowScript != null)
             {
                 _arrowScript.CanStartMoving = true;
                 _arrowScript = null;
+                
+                // Placeholder 
+                _bowSpriteRenderer.sprite = bowPlaceHolder1;
             }
         }
 
@@ -47,6 +68,8 @@ namespace Controllers
                 float angle = Mathf.Atan2(Direction.y, Direction.x) * Mathf.Rad2Deg;
                 Vector3 rotation = new Vector3(0,0, angle);
                 _arrowScript.transform.eulerAngles = rotation;
+                
+                gameObject.transform.eulerAngles = rotation;
             } 
             
             else
