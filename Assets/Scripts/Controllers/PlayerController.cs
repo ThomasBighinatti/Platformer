0948bdb44e1e019d7796a1b91f95964f172c6adf
ThomasBighinatti <thomas.bighinatti@gmail.com>
2026-03-05 17:03:47 +0100

    g rien fait

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -6,38 +6,48 @@ namespace Controllers
     public class PlayerController : MonoBehaviour
     {
     
-        //TODO empecher le joueur de rester collé au mur quand on se déplace dessus, mvt en l'air si besoin, limiter vitesse de chute
-    
-        private Vector2 _moveInput;
-        private Rigidbody2D _rb;
-        private CapsuleCollider2D playerCollider;
-        private float _coyoteTimeCounter;
-        private float _jumpBufferCounter;
-        private float _boxCastCooldownCounter = 0;
+        //TODO réorganiser tout le bordel dans fixed update ,mvt en l'air si besoin, limiter vitesse de chute, saut en 3 phases
         
-        [Header("Player")] 
+        [Header("Player Settings")] 
         [SerializeField] private float jumpStrength = 5f;
-        [SerializeField] private bool jumpButtonPressed = false;
+        [SerializeField] private float playerAcceleration = 5f;
         [SerializeField] private float playerSpeed = 5f;
-        [SerializeField] private bool grounded = true;
+        [SerializeField] private float maxSpeed = 10f;
+        [SerializeField] private float minSpeed = 3f;
         [SerializeField] private float coyoteTime = 0.1f;
         [SerializeField] private float jumpCutMultiplier = 0.5f;
         [SerializeField] private float jumpBufferTime = 0.2f;
-        [SerializeField] float boxCastCooldown = 0.1f;
-        private bool _jumpButtonReleased = false;
-
-        [Header("Misc")] 
-        [SerializeField] private float groundCheckDistance = 0.5f; 
+        [SerializeField] private float boxCastCooldown = 0.1f;
+        [Space(10f)]
         
+        [Header("Visualisation")]
+        [SerializeField] private bool grounded = true;
+        [SerializeField] private bool jumpButtonPressed = false;
+        [Space(10f)]
+        
+        [Header("Raycast Settings")]
+        [SerializeField] private float groundCheckDistance = 0.5f;
         [SerializeField] private LayerMask groundLayer;
         [SerializeField] private Vector2 boxSize = new Vector2(0.8f, 0.2f);
+        [Space(10f)]
+        
+        [Header("Friction")]
         [SerializeField] private PhysicsMaterial2D noFrictionMaterial;
         [SerializeField] private PhysicsMaterial2D frictionMaterial;
+     
+        private Vector2 _moveInput;
+        private Rigidbody2D _rb;
+        private CapsuleCollider2D _playerCollider;
+        private float _coyoteTimeCounter;
+        private float _jumpBufferCounter;
+        private float _boxCastCooldownCounter = 0;
+        private bool _jumpButtonReleased = false;
         
-
+        
+        
         private void Start()
         {
-            playerCollider = gameObject.GetComponent<CapsuleCollider2D>();
+            _playerCollider = gameObject.GetComponent<CapsuleCollider2D>();
             _rb = GetComponent<Rigidbody2D>();
         }
 
@@ -48,7 +58,6 @@ namespace Controllers
                 jumpButtonPressed = true;
                 _jumpBufferCounter = jumpBufferTime;
             }
-        
             if (context.canceled)
             {
                 _jumpButtonReleased = true;
@@ -60,34 +69,47 @@ namespace Controllers
             _moveInput = context.ReadValue<Vector2>();
         }
         
-
         private void FixedUpdate()
         {
             _jumpBufferCounter -= Time.fixedDeltaTime;
-            _boxCastCooldownCounter -= Time.fixedDeltaTime; //pauvre con que je suis j'ai ecrit =- au lieu de -=
+            _boxCastCooldownCounter -= Time.fixedDeltaTime; //j'ai ecrit "=-" au lieu de "-="...
             
             RaycastHit2D groundHit = Physics2D.BoxCast(transform.position, boxSize, 0f, Vector2.down, groundCheckDistance, groundLayer);
-            grounded = groundHit.collider != null && _boxCastCooldownCounter <= 0f; //perso au sol si raycast + si le cooldown est a 0
+            grounded = groundHit.collider is not null && _boxCastCooldownCounter <= 0f; //perso au sol si raycast + si le cooldown est a 0
             
             Vector2 targetVelocity = _rb.linearVelocity;
 
-            #region mvt
-            
+            #region Movement
             if (grounded)
             {
-                playerCollider.sharedMaterial = frictionMaterial;
+                _playerCollider.sharedMaterial = frictionMaterial;
                 _rb.sharedMaterial = frictionMaterial;
                 _coyoteTimeCounter = coyoteTime;
 
                 #region mvtSlope
                 Quaternion slopeRotation = Quaternion.FromToRotation(Vector2.up, groundHit.normal);
-                targetVelocity = slopeRotation * new Vector2(_moveInput.x * playerSpeed, 0f); //on force a 0 la velocite Y. AHHHHHHH CA EMPECHE GROUNDED D'ETRE VRAI ???????? JE PEUX METTRE UN TIMER DE 0.1 SEC AU SAUT ?
-                //Debug.Log(slopeRotation);
+                targetVelocity = slopeRotation * new Vector2(_moveInput.x * playerSpeed, 0f);
+                /* if(speed < maxSpeed)
+                    {
+                     speed += acceleration * Time.deltaTime;
+                    }
+                transform.position.x = transform.position.x + speed*Time.deltaTime;
+                
+                faire un truc comme ca mais partout en gros
+                    */
+                // Debug.Log(slopeRotation);
                 #endregion
+                
+                if (slopeRotation != new Quaternion(0, 0, 0, 1))
+                {
+                    _rb.bodyType = RigidbodyType2D.Kinematic;
+                    _rb.linearVelocity = Vector2.zero;
+                }
             }
             else //mvt en l'air
             {
-                playerCollider.sharedMaterial = noFrictionMaterial;
+                _rb.bodyType = RigidbodyType2D.Dynamic;
+                _playerCollider.sharedMaterial = noFrictionMaterial;
                 _rb.sharedMaterial = noFrictionMaterial;
                 targetVelocity = new Vector2(_moveInput.x * playerSpeed, targetVelocity.y);
                 _coyoteTimeCounter -= Time.fixedDeltaTime; //fixeddeltatime prcq on est dans fixedupdate
@@ -95,7 +117,7 @@ namespace Controllers
             }
             #endregion
             
-            #region saut
+            #region Jump
             if (_coyoteTimeCounter > 0f && _jumpBufferCounter > 0f)
             {
                 targetVelocity = new Vector2(targetVelocity.x, jumpStrength);
@@ -106,7 +128,7 @@ namespace Controllers
             }
             #endregion
             
-            #region jumpcut
+            #region Jumpcut
             if (_jumpButtonReleased)
             {
                 if (targetVelocity.y > 0f)
@@ -116,7 +138,7 @@ namespace Controllers
                 _jumpButtonReleased = false;
             }
             #endregion
-            Debug.Log(targetVelocity.y); //se met a 0 dans l'editeur alors pk ca descent
+            
             _rb.linearVelocity = targetVelocity;
         }
     
@@ -126,7 +148,4 @@ namespace Controllers
             Gizmos.DrawWireCube(transform.position + Vector3.down * groundCheckDistance, boxSize);
         }
     }
-    /* L'idée est d'ajouter un petit chronomètre (un "cooldown" de saut, par exemple 0.1 seconde).
-     Quand tu valides un saut dans ta région #saut, tu lances ce chronomètre. Et tout en haut de ton script,
-    tu modifies ton BoxCast pour qu'il ne puisse renvoyer true que si ce chronomètre est tombé à zéro. (il m'a dit de la merde ca fonctionne pas) */
 }
