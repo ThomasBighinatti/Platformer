e4ab1ae7838273017d0c81b055a2a575384ad18e
Maxence <maxence.desceliers@gmail.com>
2026-03-14 17:25:20 +0100

    DO NOT TAKE !! editor errors in code
    c'est pour jacques

---
+++ Assets/Scripts/EditorWindows/PlayerEditorWindow.cs
@@ -0,0 +1,70 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+using Datas;
+
+namespace EditorWindows
+{
+    public class PlayerEditorWindow : EditorWindow
+    {
+        
+
+        [SerializeField] private PlayerData dataCible;
+
+        [MenuItem("Tools/PlayerModifier")]
+        public static void ShowWindow()
+        {
+            GetWindow<PlayerEditorWindow>("PlayerModifier");
+        }
+
+        private void OnGUI()
+        {
+            GUILayout.Label("Player modifier", EditorStyles.boldLabel);
+            EditorGUILayout.Space();
+
+            dataCible = (PlayerData)EditorGUILayout.ObjectField("Entity to modify", dataCible, typeof(PlayerData), false);
+
+            if (dataCible is not null)
+            {
+                EditorGUILayout.BeginVertical("box");
+            
+                dataCible.PlayerSpeed = EditorGUILayout.IntField("Speed", dataCible.speed);
+
+                EditorGUILayout.BeginHorizontal();
+                if (GUILayout.Button("-10")) dataCible.speed -= 10;
+                if (GUILayout.Button("+10")) dataCible.speed += 10;
+                EditorGUILayout.EndHorizontal();
+
+                EditorGUILayout.EndVertical();
+
+                if (GUI.changed)
+                {
+                    EditorUtility.SetDirty(dataCible);
+                    AssetDatabase.SaveAssets();
+                }
+            }
+            else
+            {
+                EditorGUILayout.HelpBox("Glisse un ScriptableObject 'Speed data' chef", MessageType.Info);
+            }
+        }
+    
+        private void OnEnable()
+        {
+            Prefill();
+        }
+    
+        private void Prefill()
+        {
+            string[] guids = AssetDatabase.FindAssets("t:EntityData");
+
+            if (guids.Length > 0)
+            {
+                string path = AssetDatabase.GUIDToAssetPath(guids[0]);
+                dataCible = AssetDatabase.LoadAssetAtPath<EntityData>(path);
+            }
+        }
+    
+
+    }
+}
