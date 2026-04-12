62ea9ac241b1db1aace863e48046a7b74639461c
Maxence <maxence.desceliers@gmail.com>
2026-04-13 01:45:22 +0200

    papi llon pour papi julien

--- Assets/Scripts/Controllers/ButterflyController.cs
+++ Assets/Scripts/Controllers/ButterflyController.cs
@@ -1,3 +1,5 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Controllers
@@ -9,25 +11,84 @@ namespace Controllers
         [SerializeField] private float speed = 1;
         [SerializeField] private float distance = 1;
         private float _time;
-        private float _xCoords;
-        private float _yCoords;
-        private Vector2 _offset;
 
-        private float _previousXPosition;
-        private bool IsMovingRight => bfVisual.transform.localPosition.x > _previousXPosition;
+        [SerializeField] private GameObject playerPointer;
+        [SerializeField] private GameObject butterflyPointer;
+        [SerializeField] private GameObject pointerRef;
+        [SerializeField] private float butterflyPointerOffset;
+        [SerializeField] private float shapeModifier = 0.5f;
+
+        public enum ButterflyState
+        {
+            Idle = 1,
+            Trans = 2,
+            Prepared = 4,
+            Shoot = 8
+        }
+
+        public static ButterflyState CurrentButterflyState { get; private set; } = ButterflyState.Idle;
+
+        public static void ToIdleState() => CurrentButterflyState = ButterflyState.Idle;
+        public static void ToTransState() => CurrentButterflyState = ButterflyState.Trans;
+        public static void ToPreparedState() => CurrentButterflyState = ButterflyState.Prepared;
+        public static void ToShootState() => CurrentButterflyState = ButterflyState.Shoot;
 
         private void Update()
         {
-            _previousXPosition = bfVisual.transform.localPosition.x;
+            if (CurrentButterflyState == ButterflyState.Idle)
+            {
+                ButterFlyPointerRefresh();
+
+                MoveButterfly();
+
+                MoveButterflyVisual();
+            }
+
+            if (CurrentButterflyState == ButterflyState.Prepared)
+            {
+                
+            }
+        }
+
+        private void ButterFlyPointerRefresh()
+        {
+            pointerRef.transform.position = playerPointer.transform.position;
+            butterflyPointer.transform.localPosition = new Vector2(
+                pointerRef.transform.localPosition.x,
+                pointerRef.transform.localPosition.y * shapeModifier + butterflyPointerOffset
+            );
+        }
+
+        private Vector2 _velocity;
+        [SerializeField] private float smoothTime = 0.3f;
+
+        private void MoveButterfly()
+        {
+            Vector2 newPosition = Vector2.SmoothDamp(
+                transform.position, 
+                butterflyPointer.transform.position, 
+                ref _velocity,
+                smoothTime);
+            transform.position = newPosition;
+        }
+        
+        private bool IsMovingRight => bfVisual.transform.position.x > _previousXWorldPosition;
+        private float _previousXWorldPosition;
+        
+        private void MoveButterflyVisual()
+        {
             _time += Time.deltaTime * speed;
-            _xCoords = Mathf.PerlinNoise(Mathf.Cos(_time) * 1.5f + 10f, Mathf.Sin(_time) * 1.5f + 10f);
-            _yCoords = Mathf.PerlinNoise(Mathf.Sin(_time) * 1.5f + 20f, Mathf.Cos(_time) * 1.5f + 20f);
+            float timeY = _time * 1.23f;
+            float xCoords = Mathf.PerlinNoise(Mathf.Cos(_time) * 1.5f + 10f, Mathf.Sin(_time) * 1.5f + 10f) * 2 - 1;
+            float yCoords = Mathf.PerlinNoise(Mathf.Sin(timeY) * 1.5f + 20f, Mathf.Cos(timeY) * 1.5f + 20f) * 2 - 1;
 
-            _offset = new Vector2(_xCoords, _yCoords) * distance;
+            Vector2 offset = new Vector2(xCoords, yCoords) * distance;
 
-            bfVisual.transform.localPosition = _offset;
+            bfVisual.transform.localPosition = offset;
             
             bfVisual.transform.localScale = new Vector3(IsMovingRight ? 1f : -1f, 1f, 1f);
+            
+            _previousXWorldPosition = bfVisual.transform.position.x;
         }
     }
 }
