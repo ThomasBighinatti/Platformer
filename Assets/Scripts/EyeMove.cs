d11248291deaba71a8d838cc14e323fada055e60
maxenced-dev <maxence.desceliers@gmail.com>
2026-04-28 10:17:57 +0200

    start eye

--- Assets/Scripts/EyeMove.cs
+++ Assets/Scripts/EyeMove.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using Managers;
 using UnityEngine;
@@ -9,38 +10,46 @@ public class EyeMove : MonoBehaviour
 
     [SerializeField] private GameObject pupil;
     [SerializeField] private float centerDistance;
-    [SerializeField, Range(0, 1)] private float offsetDistance;
     [SerializeField, Range(0, 1)] private float offsetDistanceOnLook;
     [SerializeField] private float randomTimeWaitMin;
     [SerializeField] private float randomTimeWaitMax;
-    [SerializeField, Range(0, 1)] private float probaLookPlayer;
     
-    [SerializeField] private float detectionDistance;
+    //[SerializeField, Range(0, 1)][Obsolete] private float probaLookPlayer;
+    //[SerializeField, Range(0, 1)][Obsolete] private float offsetDistance;
+    
+    //[SerializeField] private float detectionDistance;
     private GameObject _player;
     private Vector3 _pupilCenter;
     
     private void Start()
-    {
+    { 
         _player = LevelManager.Instance.player;
         _pupilCenter = pupil.transform.position;
         StartCoroutine(WaitForLookCycle());
-        Debug.DrawRay(transform.position,Vector3.left * detectionDistance, Color.brown,float.MaxValue);
+        //Debug.DrawRay(transform.position,Vector3.left * detectionDistance, Color.brown,float.MaxValue);
     }
 
-    private bool IsInRange => Vector2.Distance(_player.transform.position, transform.position) <= detectionDistance;
+    //private bool IsInRange => Vector2.Distance(_player.transform.position, transform.position) <= detectionDistance;
 
-    private void LookTowardsPlayer()
+    /*private void LookTowardsPlayer()
     {
         pupil.transform.position = _pupilCenter + (_player.transform.position - pupil.transform.position).normalized * centerDistance;
         pupil.transform.position += (Vector3)Random.insideUnitCircle * offsetDistanceOnLook;
-    }
+    }*/
 
-    private void VariationCenterLook()
+    /*private void VariationCenterLook()
     {
         pupil.transform.position = _pupilCenter + (Vector3)Random.insideUnitCircle * offsetDistance;
-    }
+    }*/
 
     private IEnumerator WaitForLookCycle()
+    {
+        //LookTowardsPlayer();
+        yield return new WaitForSeconds(Random.Range(randomTimeWaitMin, randomTimeWaitMax));
+        StartCoroutine(WaitForLookCycle());
+    }
+    
+    /*private IEnumerator WaitForLookCycle()
     {
         if (IsInRange)
         {
@@ -55,6 +64,6 @@ public class EyeMove : MonoBehaviour
         VariationCenterLook();
         yield return new WaitForSeconds(Random.Range(randomTimeWaitMin, randomTimeWaitMax));
         StartCoroutine(WaitForLookCycle());
-    }
+    }*/
     
 }
