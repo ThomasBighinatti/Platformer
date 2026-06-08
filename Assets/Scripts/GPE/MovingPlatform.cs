ff70d53c45e8b34e3b055be932ddef7b2d7528fb
maxenced-dev <maxence.desceliers@gmail.com>
2026-06-08 16:55:29 +0200

    ça marche pas

--- Assets/Scripts/GPE/MovingPlatform.cs
+++ Assets/Scripts/GPE/MovingPlatform.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections.Generic;
 using Managers;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace GPE
 {
@@ -11,8 +13,17 @@ namespace GPE
         
         [Header("Platform Settings")]
         [SerializeField] private MovingPlatformSettings settings;
-        [Serializable]
+
+        [Space(10f)] [Header("Visual Settings")] 
+        [SerializeField] private float lengthScale;
+        [SerializeField] private float heightScale;
+        // two by two
+        [SerializeField] private List<Sprite> armVertical;
+        [SerializeField] private List<Sprite> armHorizontal;
+        [SerializeField] private List<Sprite> hands;
+        [SerializeField] private List<Sprite> ends;
         
+        [Serializable] 
         private struct MovingPlatformSettings
         {
             public Direction direction;
@@ -22,6 +33,8 @@ namespace GPE
     
         private Vector2 _initialPos;
         private Vector2 _targetPos;
+
+        private GameObject _parent;
 
         #region Direction GD
     
@@ -49,14 +62,62 @@ namespace GPE
 
         private void Start()
         {
-            Debug.DrawRay(transform.position,GetDirection() * settings.distance,Color.red,float.MaxValue);
             _initialPos = transform.position;
             _targetPos = _initialPos + GetDirection() * settings.distance;
+            
+            _parent = new GameObject(gameObject.name + "Parent");
+            gameObject.transform.SetParent(_parent.transform);
+            Debug.DrawRay(transform.position,GetDirection() * settings.distance,Color.red,float.MaxValue);
+            
             GameManager.Instance.Subscribe(this);
+
+            switch (settings.direction)
+            {
+                case Direction.Up:
+                    SpawnVisuals(heightScale, armVertical, hands[0], hands[1], ends[0], ends[1], GetDirection());
+                    break;
+                case Direction.Right:
+                    SpawnVisuals(lengthScale, armHorizontal, hands[2], hands[3], ends[2], ends[3], GetDirection());
+                    break;
+                case Direction.Down:
+                    SpawnVisuals(heightScale, armVertical, hands[4], hands[5], ends[4], ends[5], GetDirection());
+                    break;
+                case Direction.Left:
+                    SpawnVisuals(lengthScale, armHorizontal, hands[6], hands[7], ends[6], ends[7], GetDirection());
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+        
+        private void SpawnVisuals(float scale, List<Sprite> armSprites, Sprite handA, Sprite handB, Sprite endA, Sprite endB, Vector2 direction)
+        {
+            float halfScale = scale / 2f - 1f;
+            
+            GameObject hand = new GameObject("Hand");
+            hand.transform.SetParent(transform);
+            hand.transform.localPosition = direction * halfScale;
+            Debug.Log(direction);
+            Debug.Log(halfScale);
+            Debug.Log(direction * halfScale);
+            hand.AddComponent<SpriteRenderer>().sprite = Random.Range(0, 2) == 0 ? handA : handB;
+            
+            for (int i = 0; i < (int)settings.distance; i++)
+            {
+                GameObject arm = new GameObject("Arm " + i);
+                arm.transform.SetParent(transform);
+                arm.transform.position = _initialPos + direction * (halfScale + i + 0.5f);
+                arm.transform.SetParent(_parent.transform);
+                arm.AddComponent<SpriteRenderer>().sprite = armSprites[i % armSprites.Count];
+            }
+            
+            GameObject end = new GameObject("End");
+            end.transform.SetParent(transform);
+            end.transform.position = _initialPos + direction * (halfScale + settings.distance + 0.5f);
+            end.AddComponent<SpriteRenderer>().sprite = Random.Range(0, 2) == 0 ? endA : endB;
         }
 
         private void OnDisable() => GameManager.Instance.Unsubscribe(this);
-        
 
         private void FixedUpdate()
         {
