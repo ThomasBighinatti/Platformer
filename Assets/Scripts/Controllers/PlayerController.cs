dabb9c9df46d640ef3e402a1b7494b2fdd65acca
maxenced-dev <maxence.desceliers@gmail.com>
2026-06-09 19:13:28 +0200

    cleanuperu

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -84,7 +84,6 @@ namespace Controllers
             }
             _explosionAnimator = explosionAnimator;
             _explosionAnimator.gameObject.SetActive(false);
-
         }
 
         private void FixedUpdate()
